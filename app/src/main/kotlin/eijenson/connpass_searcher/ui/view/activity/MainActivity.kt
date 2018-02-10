c9cd916c2e0a486cb9ff5b012a90bed61d19b69e
kob <mako0844420@gmail.com>
2018-02-10 21:30:46 +0900

    refactor: add presenter

--- app/src/main/kotlin/eijenson/connpass_searcher/ui/view/activity/MainActivity.kt
+++ app/src/main/kotlin/eijenson/connpass_searcher/ui/view/activity/MainActivity.kt
@@ -2,16 +2,21 @@ package eijenson.connpass_searcher.ui.view.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.EditorInfo
-import com.google.gson.Gson
+import android.widget.Toast
 import eijenson.connpass_searcher.R
-import eijenson.connpass_searcher.repository.api.response.ResultEventJson
-import eijenson.connpass_searcher.repository.api.response.mapping.toResultEvent
+import eijenson.connpass_searcher.repository.api.EventRepositoryImpl
+import eijenson.connpass_searcher.repository.entity.RequestEvent
 import eijenson.connpass_searcher.ui.view.adapter.EventListAdapter
+import eijenson.model.Event
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
-
+class MainActivity : Activity(), MainContent.View {
+    lateinit var presenter: MainContent.Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +26,50 @@ class MainActivity : Activity() {
             }
             false
         }
-        val inputStream = assets.open("result.json")
-        val list = inputStream.reader().use { Gson().fromJson(it, ResultEventJson::class.java).toResultEvent().events }
-        list_result.adapter = EventListAdapter(this, list)
+
+        presenter = MainPresenter(this)
+        presenter.search()
+        //val inputStream = assets.open("result.json")
+        //val list = inputStream.reader().use { Gson().fromJson(it, ResultEventJson::class.java).toResultEvent().events }
+    }
+
+    override fun showSearchResult(eventList: List<Event>) {
+        list_result.adapter = EventListAdapter(this, eventList)
+    }
+
+    override fun showSearchErrorToast() {
+        Toast.makeText(this, "通信失敗", Toast.LENGTH_SHORT).show()
+    }
+}
+
+interface MainContent {
+    interface View {
+        fun showSearchResult(eventList: List<Event>)
+        fun showSearchErrorToast()
+
+    }
+
+    interface Presenter {
+        fun search()
+
+    }
+}
+
+class MainPresenter(private val view: MainContent.View) : MainContent.Presenter {
+
+
+    override fun search() {
+        EventRepositoryImpl().getEvent(RequestEvent(keyword = "Android"))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribeBy(
+                        onNext = {
+                            view.showSearchResult(it.events)
+                        },
+                        onError = {
+                            Log.d("MainActivity", "search", it)
+                            view.showSearchErrorToast()
+                        }
+                )
     }
 }
