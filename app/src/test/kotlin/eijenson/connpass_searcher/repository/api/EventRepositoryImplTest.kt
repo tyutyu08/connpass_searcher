f6d48386e5961f52749262d26d70147b478127bf
kob <mako0844420@gmail.com>
2018-02-10 21:33:32 +0900

    refactor: change api result

--- app/src/test/kotlin/eijenson/connpass_searcher/repository/api/EventRepositoryImplTest.kt
+++ app/src/test/kotlin/eijenson/connpass_searcher/repository/api/EventRepositoryImplTest.kt
@@ -2,13 +2,15 @@ package eijenson.connpass_searcher.repository.api
 
 import eijenson.connpass_searcher.repository.entity.RequestEvent
 import io.reactivex.rxkotlin.subscribeBy
+import org.junit.Ignore
 import org.junit.Test
 
 class EventRepositoryImplTest {
     @Test
+    @Ignore
     fun addition_isCorrect() {
         val repository = EventRepositoryImpl()
-        repository.getEvent(RequestEvent(keyword = "hiuhkjhkjhkhiuhkjhiuhkjhuih")).subscribeBy(
+        repository.getEvent(RequestEvent(keyword = "Android")).subscribeBy(
                 onNext = {
                     println(it.toString())
                     assert(it != null)
