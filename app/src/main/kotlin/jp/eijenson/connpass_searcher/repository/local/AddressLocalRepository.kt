ebb7774c289c6b18042cf95606335748367e8131
kob <makoto.kobayashi@jmty.jp>
2018-04-09 13:23:13 +0900

    🏥:repair exception when geocoder throw IOException

--- app/src/main/kotlin/jp/eijenson/connpass_searcher/repository/local/AddressLocalRepository.kt
+++ app/src/main/kotlin/jp/eijenson/connpass_searcher/repository/local/AddressLocalRepository.kt
@@ -2,19 +2,26 @@ package jp.eijenson.connpass_searcher.repository.local
 
 import android.content.Context
 import android.location.Geocoder
+import timber.log.Timber
+import java.io.IOException
 import java.util.*
 
 /**
  * Created by kobayashimakoto on 2018/04/06.
  */
 class AddressLocalRepository(context: Context) {
-    val geocoder= Geocoder(context, Locale.getDefault())
+    val geocoder = Geocoder(context, Locale.getDefault())
 
-    fun getAddress(latitube:Double,longitube:Double):String{
-        val addresses = geocoder.getFromLocation(latitube,longitube,1)
-        for (address in addresses) {
-            return address.adminArea
+    fun getAddress(latitube: Double, longitube: Double): String {
+        try {
+            val addresses = geocoder.getFromLocation(latitube, longitube, 1)
+            for (address in addresses) {
+                return address.adminArea
+            }
+            return ""
+        } catch (e: IOException) {
+            Timber.d(e)
+            return ""
         }
-        return ""
     }
 }
