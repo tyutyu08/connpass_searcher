e7f170fa44d03ec013f0747d3a774a062ee24c7b
kob <makoto.kobayashi@jmty.jp>
2018-04-16 13:57:33 +0900

    ➕: add job service because use notification

---
+++ app/src/main/kotlin/jp/eijenson/connpass_searcher/ui/service/MyJobService.kt
@@ -0,0 +1,17 @@
+package jp.eijenson.connpass_searcher.ui.service
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+/**
+ * Created by makoto.kobayashi on 2018/04/16.
+ */
+class MyJobService : JobService() {
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
