669450f150cd443d8eeba7ed72d4f112939dc30f
kob <makoto.kobayashi@jmty.jp>
2018-04-20 12:56:39 +0900

    🐞:crash geocoder not null

--- app/src/main/kotlin/jp/eijenson/connpass_searcher/repository/db/AddressLocalRepository.kt
+++ app/src/main/kotlin/jp/eijenson/connpass_searcher/repository/db/AddressLocalRepository.kt
@@ -1,6 +1,7 @@
 package jp.eijenson.connpass_searcher.repository.db
 
 import android.content.Context
+import android.location.Address
 import android.location.Geocoder
 import timber.log.Timber
 import java.io.IOException
@@ -10,15 +11,12 @@ import java.util.*
  * Created by kobayashimakoto on 2018/04/06.
  */
 class AddressLocalRepository(context: Context) {
-    val geocoder = Geocoder(context, Locale.getDefault())
+    val geocoder = Geocoder(context, Locale.JAPAN)
 
     fun getAddress(latitube: Double, longitube: Double): String {
         try {
-            val addresses = geocoder.getFromLocation(latitube, longitube, 1)
-            for (address in addresses) {
-                return address.adminArea
-            }
-            return ""
+            val addresses: List<Address> = geocoder.getFromLocation(latitube, longitube, 1)
+            return addresses.firstOrNull()?.adminArea ?: ""
         } catch (e: IOException) {
             Timber.d(e)
             return ""
