786f87e7379bbc54c92cee46e1927fd916555c6a
eiji <m.kobayashi@binc.jp>
2019-12-18 20:12:48 +0900

    feature: add http log fix: send invalid request

--- infra/src/main/java/xyz/eijenson/infra/repository/api/EventApiRepository.kt
+++ infra/src/main/java/xyz/eijenson/infra/repository/api/EventApiRepository.kt
@@ -1,17 +1,19 @@
 package xyz.eijenson.infra.repository.api
 
 import io.reactivex.Single
-import xyz.eijenson.domain.repository.EventRemoteRepository
-import xyz.eijenson.infra.repository.api.entity.mapping.toRequestEventJson
-import xyz.eijenson.infra.repository.api.entity.response.mapping.toResultEvent
-import xyz.eijenson.infra.api.ApiV1
 import jp.eijenson.model.RequestEvent
 import jp.eijenson.model.ResultEvent
 import jp.eijenson.model.SearchHistory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import xyz.eijenson.domain.repository.EventRemoteRepository
+import xyz.eijenson.infra.BuildConfig
+import xyz.eijenson.infra.api.ApiV1
+import xyz.eijenson.infra.repository.api.entity.mapping.toRequestEventJson
+import xyz.eijenson.infra.repository.api.entity.response.mapping.toResultEvent
 import java.util.Date
 
 class EventApiRepository : EventRemoteRepository {
@@ -19,7 +21,11 @@ class EventApiRepository : EventRemoteRepository {
     private val eventApi: ApiV1
 
     init {
-        val client = OkHttpClient.Builder().build()
+        val logging = HttpLoggingInterceptor()
+        logging.level = if(BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
         retrofit = Retrofit.Builder()
             .baseUrl("https://connpass.com")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
--- infra/src/main/java/xyz/eijenson/infra/repository/api/entity/mapping/RequestEventMapping.kt
+++ infra/src/main/java/xyz/eijenson/infra/repository/api/entity/mapping/RequestEventMapping.kt
@@ -1,8 +1,8 @@
 package xyz.eijenson.infra.repository.api.entity.mapping
 
-import xyz.eijenson.infra.repository.api.entity.RequestEventJson
 import jp.eijenson.model.RequestEvent
 import jp.eijenson.model.SearchHistory
+import xyz.eijenson.infra.repository.api.entity.RequestEventJson
 import java.util.Date
 
 /**
@@ -31,21 +31,21 @@ fun RequestEventJson.toSearchHistory(): SearchHistory {
 
 fun RequestEvent.toRequestEventJson(): RequestEventJson {
     return RequestEventJson(
-        eventId ?: -1,
-        keyword ?: "",
-        keywordOr ?: "",
-        ym ?: -1,
-        ymd ?: -1,
-        nickname ?: "",
-        ownerNickname ?: "",
-        seriesId ?: -1,
-        start ?: -1,
-        order ?: -1,
-        count ?: -1,
-        format ?: "",
+        convertColumn(eventId),
+        convertColumn(keyword),
+        convertColumn(keywordOr),
+        convertColumn(ym),
+        convertColumn(ymd),
+        convertColumn(nickname),
+        convertColumn(ownerNickname),
+        convertColumn(seriesId),
+        convertColumn(start),
+        convertColumn(order),
+        convertColumn(count),
+        convertColumn(format),
         prefecture
     )
 }
 
-private fun convertColumn(int: Int): Int? = if (int != -1) int else null
-private fun convertColumn(str: String): String? = if (str != "") str else null
+private fun convertColumn(int: Int?): Int? = if (int != null && int != -1) int else null
+private fun convertColumn(str: String?): String? = if (str != null && str != "") str else null
